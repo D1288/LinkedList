f168dba35d01579e675e03f16a59facbe2f811bf
Daniel Metz <107957273+D1288@users.noreply.github.com>
2023-05-12 18:50:10 -0700

    added in methods for contains, find, toString, insertAt, and removeAt

--- index.js
+++ index.js
@@ -60,12 +60,106 @@ class LinkedList {
         }
     }
 
-    at(index){
-        if(index > 0 && index > this.size){
+    at(index) {
+        if (index > 0 && index > this.size) {
             return "Index is out of Bounds"
-        } else{
-            
+        } else {
+            let current = this.head;
+            let count = 0;
+            while (current) {
+                if (count === index) {
+                    console.log(current)
+                } else {
+                    count++;
+                    current = current.next
+                }
+            }
         }
+    }
+
+    pop(){
+        if(!this.head){
+            return "List is Empty"
+        } else{
+            let current = this.head;
+            let previous
+            while(current.next){
+                previous = current
+                current = current.next
+            }
+            if(previous){
+                previous.next = null;
+            } else{
+                this.head = null;
+            }
+        }
+        this.size --
+    }
+
+    contains(value){
+        let current = this.head;
+        while(current){
+            if(current.value === value){
+                return true
+            }
+            current = current.next;
+        }
+        return false
+    }
+
+    find(value){
+        let current = this.head
+        let index = 0;
+        while (current){
+            if(current.value === value){
+                return index
+            } else{
+                current = current.next;
+                index++
+            }
+        }
+        return null;
+    }
+
+    toString(){
+        let current = this.head;
+        while(current){
+            console.log(current)
+            current = current.next
+        }
+    }
+
+    insertAt(value, index){
+        const node = new Node(value)
+        let previous;
+
+        let current = this.head;
+        let count = 0;
+
+        while (count < index){
+            previous = current;
+            count ++;
+            current = current.next
+        }
+
+        node.next = current;
+        previous.next = node;
+        this.size++
+    }
+
+    removeAt(index){
+        let current = this.head;
+        let previous;
+        let count = 0;
+
+        while(count < index){
+            previous = current;
+            count ++;
+            current = current.next
+        }
+
+        previous.next = current.next;
+        this.size --;
     }
 
 
